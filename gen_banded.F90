691945d7b00e5d7115263758a2c8cd4632fb3a8a
Ed D'Azevedo <dazevedoef@ornl.gov>
2019-12-09 12:49:31 -0500

    initial checkin

---
+++ gen_banded.F90
@@ -0,0 +1,35 @@
+      subroutine gen_banded(n,kl,ku, A, ldA )
+      implicit none
+! % ----------------------
+! % generate banded matrix of size n by n
+! % with lower bandwidth kl and upper bandwidth ku
+! % ----------------------
+      integer, intent(in) :: n, kl, ku, ldA
+      integer, parameter :: dp = selected_real_kind(15,100)
+      integer, parameter :: sp = selected_real_kind(15,100)
+      integer, parameter :: wp = dp
+      complex(kind=wp), intent(inout) :: A(ldA,n)
+
+      integer :: i,j
+      real(kind=wp) :: x_re(n), x_im(n)
+      complex(kind=wp) :: aij
+
+      do j=1,n
+	 call random_number( x_re(1:n) )
+	 call random_number( x_im(1:n) )
+	 x_re(1:n) = 2*x_re(1:n) - 1
+	 x_im(1:n) = 2*x_im(1:n) - 1
+	 do i=1,n
+	   aij = cmplx( x_re(i), x_im(i), kind=wp)
+           if (i - j > kl) then
+	       aij = 0
+	   endif
+	   if (j - i > ku) then
+	       aij = 0
+	   endif
+	   A(i,j) = aij
+	 enddo
+      enddo
+
+      return
+      end subroutine gen_banded
