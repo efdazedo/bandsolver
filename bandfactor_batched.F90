9bcef35d98955d9cb725cdd26d2279984c9bf9af
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2020-01-16 16:06:22 -0500

    use fixed format

--- bandfactor_batched.F90
+++ bandfactor_batched.F90
@@ -1,4 +1,4 @@
-      subroutine bandfactor_batched(n,A,lda,old2new,kl_array,ku_array,                        &
+      subroutine bandfactor_batched(n,A,lda,old2new,kl_array,ku_array,   &
      &              info_array, batchCount)
       implicit none
       integer, intent(in) :: n, lda, batchCount
@@ -15,7 +15,8 @@
       ku = 0
 !$omp parallel do private(ibatch,kl,ku,info)
       do ibatch=1,batchCount
-         call bandfactor(n,A(1,1,ibatch),lda,old2new(1,ibatch),kl,ku,info)
+         call bandfactor(n,A(1,1,ibatch),lda,                            &
+     &                   old2new(1,ibatch),kl,ku,info)
          kl_array(ibatch) = kl
          ku_array(ibatch) = ku
          info_array(ibatch) = info
